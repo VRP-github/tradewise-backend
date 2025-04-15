92fe81deefbb6d32e0ddd311e09f8711cadb8fcc
devarsh8801 <125999721+devarsh8801@users.noreply.github.com>
2025-04-15 17:59:00 -0500

    Update urls.py

--- tradewise/tradewise/urls.py
+++ tradewise/tradewise/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from trade_log.views import home
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('upload_file/', include('upload_file.urls')),  
     path('portfolio_organizer/', include('portfolio_organizer.urls')),
